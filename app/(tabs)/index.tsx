46fdaaeb6ebe07334007c54a8a87df7f8b9f5fc1
Filip <zdjelarfilip@gmail.com>
2025-01-13 15:00:28 +0100

    initial project setup

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text className="font-2xl t mt-24">1st tab </Text>
+    </View>
+  );
+}
